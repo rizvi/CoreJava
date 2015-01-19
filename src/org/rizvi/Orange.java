6242663b132c41e9714356de10cca880d113df9c
rizvi <rizvi@bjitgroup.com>
2015-01-19 15:55:42 +0600

    "Orange.java is created"

---
+++ src/org/rizvi/Orange.java
@@ -0,0 +1,10 @@
+package org.rizvi;
+
+public class Orange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Orange class");
+	}
+
+}
