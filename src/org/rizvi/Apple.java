b5b9c74aacbee307b53180761064739b2c765d30
Rizvi <abu.rizvi@bjitgroup.com>
2015-08-14 09:44:02 +0600

    Mun# Remove Unused code

--- src/org/rizvi/Apple.java
+++ src/org/rizvi/Apple.java
@@ -1,7 +1,5 @@
 package org.rizvi;
 
-import java.util.*;
-
 public class Apple{
 	public static void main(String args[]){
 		System.out.println("This is an apple...");
--- src/org/rizvi/Orange.java
+++ src/org/rizvi/Orange.java
@@ -3,7 +3,6 @@ package org.rizvi;
 public class Orange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("This is Orange class");
 	}
 
